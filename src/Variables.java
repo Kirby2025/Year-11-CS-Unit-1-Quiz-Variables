60ae89cf0daff2187c978bc46ae823a8c2223899
kirby <kirbyb2025@student.cis.edu.hk>
2023-02-15 14:51:39 +0800

    completed

--- src/Variables.java
+++ src/Variables.java
@@ -2,31 +2,33 @@ public class Variables {
 
     public static void main(String[] args) {
 
-        // 1.
+        int age = 33;
 
-        // 2.
+        System.out.println(age);
 
-        // 3.
+        double balence = 18.89;
 
-        // 4.
+        int dollars = (int)balence;
+        System.out.println(dollars);
 
-        // 5.
+        int dollarsRounded = (int)balence+1;
+        System.out.println(dollarsRounded);
 
-        // 6.
+        char letter = 'A';
 
-        // 7.
+        System.out.println(letter);
 
-        // 8.
+        System.out.println((int)letter);
 
-        // 9.
+        letter++;
 
-        // 10.
+        System.out.println(letter);
 
-        // 11.
+        System.out.println((int)letter);
 
         // 12.
         /**
-         * [type answer here]
+         * [from question 6, we create the char variable "letter", and initialize it to 'A'. Then I printed out the value giving us "A", and after casting it to an integer, the value is 65. Next after incrementing the variable by +1, which gives us "B", and converting it to a integer again, we get the value "66". ]
          */
 
     }
